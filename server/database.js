efe98c9df15051211c72573ee12df49368249f59
Leandro F <fesin81@gmail.com>
2023-06-25 17:48:55 -0300

    ingreso nuevos componentes de barra de navegacion y tablas

--- server/database.js
+++ server/database.js
@@ -1,32 +1,28 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 // connect to the db
 dbConnectionInfo = {
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "password",
+  password: "GaiaKira",
   //connectionLimit: 5, //mysql connection pool length
-  database: "quagliano"
+  database: "clientes",
 };
 
 //create mysql connection pool
-var dbconnection = mysql.createPool(
-  dbConnectionInfo
-);
+var dbconnection = mysql.createPool(dbConnectionInfo);
 
-// Attempt to catch disconnects 
-dbconnection.on('connection', function (connection) {
-  console.log('DB Connection established');
+// Attempt to catch disconnects
+dbconnection.on("connection", function (connection) {
+  console.log("DB Connection established");
 
-  connection.on('error', function (err) {
-    console.error(new Date(), 'MySQL error', err.code);
+  connection.on("error", function (err) {
+    console.error(new Date(), "MySQL error", err.code);
   });
-  connection.on('close', function (err) {
-    console.error(new Date(), 'MySQL close', err);
+  connection.on("close", function (err) {
+    console.error(new Date(), "MySQL close", err);
   });
-
 });
-
 
 module.exports = dbconnection;
