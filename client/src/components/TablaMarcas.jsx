9e9c44c74db22f46087febbaa5988b2925eb4a24
LeanFerro <123032355+LeanFerro@users.noreply.github.com>
2023-08-29 19:07:21 -0300

    Cambios (#7)
    * agregue busqueda por marca en home y tabla clientes funcional
    
    * cambios de colores de login y tablas, nuevo boton en el login
    
    * pequños cambios de estetica
    
    * cambios esteticos
    
    * cambie nombre del database
    
    * nose
    
    * 1
    
    * agrego una nueva carpeta log y queda funcionando nuevo registro, sin funcionar bien la verificacion
    
    * agrego funcionalidad al login y algunos cambios de estetica

--- client/src/components/TablaMarcas.jsx
+++ client/src/components/TablaMarcas.jsx
@@ -20,9 +20,9 @@ const TablaMarcas = () => {
   const [marcas, setMarcas] = useState([]);
   const [filters, setFilters] = useState({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    MARCA: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    ACTA: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    NRESO: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    nombre: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    acta: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    resolucion: { value: null, matchMode: FilterMatchMode.CONTAINS },
     CLASE: { value: null, matchMode: FilterMatchMode.CONTAINS },
     FEC_VTO: { value: null, matchMode: FilterMatchMode.CONTAINS },
     FVTODU: { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -57,11 +57,14 @@ const TablaMarcas = () => {
     }));
   }, [location.search]);
 
+  const nombreCliente = location.state?.nombreCliente;
+  console.log(nombreCliente);
+
   const renderHeader = () => {
     return (
       <div>
         <div className="titulo">
-          <h1 className="nomb-emp">Votionis S.A.</h1>
+          <h1 className="nomb-emp">{nombreCliente}</h1>
         </div>
         <div className="header">
           <div className="flex justify-content-end ">
@@ -121,9 +124,11 @@ const TablaMarcas = () => {
           dataKey="id"
           header={header}
           globalFilterFields={[
-            "MARCA",
-            "ACTA",
-            "NRESO",
+
+            "nombre",
+            "acta",
+            "resolucion",
+
             "CLASE",
             "FEC_VTO",
             "FVTODU",
@@ -141,15 +146,21 @@ const TablaMarcas = () => {
           />
 
           <Column
-            field="MARCA"
+
+            field="nombre"
             header="MARCA"
             style={{ minWidth: "400px" }}
             body={(rowData) => (
-              <span style={{ paddingLeft: "10px" }}>{rowData.MARCA}</span>
+              <span style={{ paddingLeft: "10px" }}>{rowData.nombre}</span>
             )}
           />
-          <Column field="ACTA" header="ACTA" style={{ minWidth: "200px" }} />
-          <Column field="NRESO" header="NRESO" style={{ minWidth: "200px" }} />
+          <Column field="acta" header="ACTA" style={{ minWidth: "200px" }} />
+          <Column
+            field="resolucion"
+            header="NRESO"
+            style={{ minWidth: "200px" }}
+          />
+
           <Column field="CLASE" header="CLASE" style={{ minWidth: "100px" }} />
           <Column
             field="FEC_VTO"
