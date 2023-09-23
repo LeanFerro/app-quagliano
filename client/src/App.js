2df5fac9db2ed22cea52f152a826feccdc946086
LeanFerro <123032355+LeanFerro@users.noreply.github.com>
2023-09-23 02:48:49 -0300

    Jwt2 (#14)
    * cambie nombre del database
    
    * se agrego JWT, y funcion de btn salir

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  useNavigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
+import { useEffect } from "react";
 import BarraNav from "./components/BarraNav";
 import TablaMarcas from "./components/TablaMarcas";
 import Log from "./components/Login/Log";
 import Forgot from "./components/Recupero/Forgot";
 import Reset from "./components/Recupero/Reset";
+import { isAuthenticated } from "./components/helpers/auth";
+
+const MainRoutes = () => {
+  const navigate = useNavigate();
+  const isAuth = isAuthenticated();
+
+  useEffect(() => {
+    if (
+      !isAuth &&
+      !window.location.pathname.includes("olvido") &&
+      !window.location.pathname.includes("reset")
+    ) {
+      navigate("/log");
+    }
+  }, [isAuth, navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<BarraNav />}>
+        <Route index element={<Log />} />
+        <Route path="olvido" element={<Forgot />} />
+        <Route path="log" element={<Log />} />
+        <Route path="reset" element={<Reset />} />
+        <Route path="marcas" element={<TablaMarcas />} />
+        <Route path="*" element={<Outlet />} />
+      </Route>
+    </Routes>
+  );
+};
 
 function App() {
   return (
     <div className="container-fluid cont-total fondito">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<BarraNav />}>
-            <Route index element={<Log />} />
-            <Route path="marcas" element={<TablaMarcas />} />
-            <Route path="olvido" element={<Forgot />} />
-            <Route path="log" element={<Log />} />
-            <Route path="reset" element={<Reset />} />
-            <Route path="*" element={<Navigate replace to="/" />} />
-          </Route>
-        </Routes>
+        <MainRoutes />
       </BrowserRouter>
     </div>
   );
